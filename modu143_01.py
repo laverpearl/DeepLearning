bf9380e691cc63d5f4f94eebf9b125b576cf67cf
laverpearl <k2811jj@gmail.com>
2020-06-14 21:30:04 +0900

    0614 / 아직! 미완성!

--- modu143_01.py
+++ modu143_01.py
@@ -8,7 +8,7 @@ data = [[2,0], [4,0], [6,0], [8,1], [10,1], [12,1], [14,1]]
 x_data = [i[0] for i in data] #勉強した時間
 y_data = [i[1] for i in data] #合否
 
-#그래프로 나타내
+#그래프로 나타내기
 plt.scatter(x_data, y_data)
 plt.xlim(0, 15)
 plt.ylim(-.1, 1.1)
